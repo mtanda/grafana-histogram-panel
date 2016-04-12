12ecad687e837a2ef4ae4cb6bb441895f0776ab5
Mitsuhiro Tanda <mitsuhiro.tanda@gmail.com>
2016-04-12 17:47:05 +0900

    set template

--- src/histogram_ctrl.js
+++ src/histogram_ctrl.js
@@ -1,12 +1,13 @@
 import {MetricsPanelCtrl} from 'app/plugins/sdk';
 import {GraphCtrl} from 'app/plugins/panel/graph/module';
+import template from './template';
 
 export class HistogramCtrl extends GraphCtrl {
+  static template = template;
+
   /** @ngInject */
   constructor($scope, $injector, $rootScope, annotationsSrv) {
     super($scope, $injector, annotationsSrv);
     this.$rootScope = $rootScope;
   }
 }
-
-HistogramCtrl.templateUrl = 'module.html';
