18b6511eb75f69c6ac5b3daa5bae6ba8cf26fe12
Mitsuhiro Tanda <mitsuhiro.tanda@gmail.com>
2016-04-12 17:50:00 +0900

    try to fix template

--- src/histogram_ctrl.js
+++ src/histogram_ctrl.js
@@ -3,11 +3,11 @@ import {GraphCtrl} from 'app/plugins/panel/graph/module';
 import template from './template';
 
 export class HistogramCtrl extends GraphCtrl {
-  static template = template;
-
   /** @ngInject */
   constructor($scope, $injector, $rootScope, annotationsSrv) {
     super($scope, $injector, annotationsSrv);
     this.$rootScope = $rootScope;
   }
 }
+
+HistogramCtrl.template = template;
